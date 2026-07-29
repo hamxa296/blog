c3577f28ebc9fe7d4c8da02b5b53e0184ea5d80b
Haris Farooq <harisnetbackup@gmail.com>
2026-07-30 03:12:38 +0500

    Update MobileHome.tsx

--- src/components/hero/MobileHome.tsx
+++ src/components/hero/MobileHome.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
 
-import newerBg from '@/assets/randombg2.png';
+import newerBg from '@/assets/randombg2.webp';
 
 export default function MobileHome() {
   return (
