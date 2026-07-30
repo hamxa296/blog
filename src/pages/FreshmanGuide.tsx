9a3d26e5f6dfff66fa1100f307604573d3bd36b3
Haris Farooq <harisnetbackup@gmail.com>
2026-07-30 16:09:16 +0500

    Update FreshmanGuide.tsx

--- src/pages/FreshmanGuide.tsx
+++ src/pages/FreshmanGuide.tsx
@@ -18,7 +18,7 @@ import img12 from "../assets/12.jpeg";
 import img13 from "../assets/13.jpeg";
 import img14 from "../assets/14.png";
 import img15 from "../assets/15.jpg";
-import newBg from '../assets/randombg.png';
+import newBg from '../assets/cleanerbg.png';
 
 
 
