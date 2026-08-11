9fc82223eac826a751b9cdf4c8543eb4196e84e3
Haris Farooq <harisnetbackup@gmail.com>
2026-08-12 01:59:43 +0500

    UI UX updates

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
-import { CollectionSurfer, type CollectionItem } from '../components/gallery/CollectionSurfer';
+import { CollectionSurfer } from '../components/gallery/CollectionSurfer';
 import {
   getGalleryPhotos,
   uploadGalleryPhoto,
